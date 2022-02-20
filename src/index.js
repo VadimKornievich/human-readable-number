ed6f12f77d5c66175d2e8efa0e437db4dd86c6d9
unknown <you@example.com>
2022-02-20 19:26:11 +0300

    Add content Index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,136 @@
-module.exports = function toReadable (number) {
-  
+var TEN = 10;
+var ONE_HUNDRED = 100;
+var ONE_THOUSAND = 1000;
+var LESS_THAN_TWENTY = [
+    'zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten',
+    'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'
+];
+
+function isFinite(value) {
+    return !(typeof value !== 'number' || value !== value || value === Infinity || value === -Infinity);
 }
+
+var MAX_SAFE_INTEGER = 9007199254740991;
+
+var ENDS_WITH_DOUBLE_ZERO_PATTERN = /(hundred|thousand|(m|b|tr|quadr)illion)$/;
+var ENDS_WITH_TEEN_PATTERN = /teen$/;
+var ENDS_WITH_Y_PATTERN = /y$/;
+var ENDS_WITH_ZERO_THROUGH_TWELVE_PATTERN = /(zero|one|two|three|four|five|six|seven|eight|nine|ten|eleven|twelve)$/;
+var ordinalLessThanThirteen = {
+    zero: 'zeroth',
+    one: 'first',
+    two: 'second',
+    three: 'third',
+    four: 'fourth',
+    five: 'fifth',
+    six: 'sixth',
+    seven: 'seventh',
+    eight: 'eighth',
+    nine: 'ninth',
+    ten: 'tenth',
+    eleven: 'eleventh',
+    twelve: 'twelfth'
+};
+
+/**
+ * Converts a number-word into an ordinal number-word.
+ * @example makeOrdinal('one') => 'first'
+ * @param {string} words
+ * @returns {string}
+ */
+function makeOrdinal(words) {
+    // Ends with *00 (100, 1000, etc.) or *teen (13, 14, 15, 16, 17, 18, 19)
+    if (ENDS_WITH_DOUBLE_ZERO_PATTERN.test(words) || ENDS_WITH_TEEN_PATTERN.test(words)) {
+        return words + 'th';
+    }
+    // Ends with *y (20, 30, 40, 50, 60, 70, 80, 90)
+    else if (ENDS_WITH_Y_PATTERN.test(words)) {
+        return words.replace(ENDS_WITH_Y_PATTERN, 'ieth');
+    }
+    // Ends with one through twelve
+    else if (ENDS_WITH_ZERO_THROUGH_TWELVE_PATTERN.test(words)) {
+        return words.replace(ENDS_WITH_ZERO_THROUGH_TWELVE_PATTERN, replaceWithOrdinalVariant);
+    }
+    return words;
+}
+
+function replaceWithOrdinalVariant(match, numberWord) {
+    return ordinalLessThanThirteen[numberWord];
+}
+
+function isSafeNumber(value) {
+    return typeof value === 'number' && Math.abs(value) <= MAX_SAFE_INTEGER;
+}
+
+var TENTHS_LESS_THAN_HUNDRED = [
+    'zero', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'
+];
+
+/**
+ * Converts an integer into words.
+ * If number is decimal, the decimals will be removed.
+ * @example toWords(12) => 'twelve'
+ * @param {number|string} number
+ * @param {boolean} [asOrdinal] - Deprecated, use toWordsOrdinal() instead!
+ * @returns {string}
+ */
+function toReadable(number, asOrdinal) {
+    var words;
+    var num = parseInt(number, 10);
+
+    if (!isFinite(num)) {
+        throw new TypeError(
+            'Not a finite number: ' + number + ' (' + typeof number + ')'
+        );
+    }
+    if (!isSafeNumber(num)) {
+        throw new RangeError(
+            'Input is not a safe number, it’s either too large or too small.'
+        );
+    }
+    words = generateWords(num);
+    return asOrdinal ? makeOrdinal(words) : words;
+}
+
+function generateWords(number) {
+    var remainder, word,
+        words = arguments[1];
+
+    // We’re done
+    if (number === 0) {
+        return !words ? 'zero' : words.join(' ').replace(/,$/, '');
+    }
+    // First run
+    if (!words) {
+        words = [];
+    }
+    // If negative, prepend “minus”
+    if (number < 0) {
+        words.push('minus');
+        number = Math.abs(number);
+    }
+
+    if (number < 20) {
+        remainder = 0;
+        word = LESS_THAN_TWENTY[number];
+
+    } else if (number < ONE_HUNDRED) {
+        remainder = number % TEN;
+        word = TENTHS_LESS_THAN_HUNDRED[Math.floor(number / TEN)];
+        // In case of remainder, we need to handle it here to be able to add the “-”
+        if (remainder) {
+            word += ' ' + LESS_THAN_TWENTY[remainder];
+            remainder = 0;
+        }
+
+    } else if (number < ONE_THOUSAND) {
+        remainder = number % ONE_HUNDRED;
+        word = generateWords(Math.floor(number / ONE_HUNDRED)) + ' hundred';
+
+    }
+
+    words.push(word);
+    return generateWords(remainder, words);
+}
+
+module.exports = toReadable;
